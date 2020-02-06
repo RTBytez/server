d9c241c3d4106e5c5ea4ad716960c9d08736a294
CzEMC <cze@clashmc.co>
2020-02-06 14:42:09 -0500

    Made options on Engine Server

--- src/main/java/com/rtbytez/server/SocketIOServerHost.java
+++ src/main/java/com/rtbytez/server/SocketIOServerHost.java
@@ -1,6 +1,7 @@
 package com.rtbytez.server;
 
 import io.socket.engineio.server.EngineIoServer;
+import io.socket.engineio.server.EngineIoServerOptions;
 import io.socket.socketio.server.SocketIoServer;
 
 import javax.servlet.http.HttpServlet;
@@ -8,9 +9,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static io.socket.engineio.server.EngineIoServerOptions.ALLOWED_CORS_ORIGIN_ALL;
+
 public class SocketIOServerHost extends HttpServlet {
 
-    private final EngineIoServer engineIoServer = new EngineIoServer();
+    public static final EngineIoServerOptions OPTIONS = EngineIoServerOptions.newFromDefault();
+
+    static {
+        OPTIONS.setCorsHandlingDisabled(false);
+        OPTIONS.setPingTimeout(5000);
+        OPTIONS.setPingInterval(25000);
+        OPTIONS.setAllowedCorsOrigins(ALLOWED_CORS_ORIGIN_ALL);
+        OPTIONS.setMaxTimeoutThreadPoolSize(20);
+        OPTIONS.lock();
+    }
+
+    private final EngineIoServer engineIoServer = new EngineIoServer(OPTIONS);
     private final SocketIoServer socketIoServer = new SocketIoServer(engineIoServer);
 
     @Override
