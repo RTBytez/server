3243c91d7b77e8bd144f1536ae60c129559c4191
CzEMC <cze@clashmc.co>
2020-02-26 14:32:08 -0500

    Very small formatting fix

--- src/main/java/com/rtbytez/server/events/io/ConnectionEvent.java
+++ src/main/java/com/rtbytez/server/events/io/ConnectionEvent.java
@@ -8,6 +8,6 @@ import io.socket.socketio.server.SocketIoSocket;
 public class ConnectionEvent extends SocketEventHandler {
 
     public void exec(SocketIoServer server, SocketIoNamespace namespace, SocketIoSocket socket) {
-    socket.send("Pong");
+        socket.send("Pong");
     }
 }
