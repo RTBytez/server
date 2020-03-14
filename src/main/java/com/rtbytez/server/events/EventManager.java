bf3a44df2fba9438f7d70dc7473b491001a9bc81
CzEMC <cze@clashmc.co>
2020-03-14 04:42:05 -0400

    Netty IO Re-Write Library Continued

--- src/main/java/com/rtbytez/server/events/EventManager.java
+++ src/main/java/com/rtbytez/server/events/EventManager.java
@@ -3,6 +3,7 @@ package com.rtbytez.server.events;
 import com.corundumstudio.socketio.AckRequest;
 import com.corundumstudio.socketio.listener.EventInterceptor;
 import com.corundumstudio.socketio.transport.NamespaceClient;
+import com.rtbytez.server.Debug;
 import com.rtbytez.server.peer.Peer;
 import com.rtbytez.server.peer.PeerManager;
 
@@ -12,6 +13,7 @@ public class EventManager implements EventInterceptor {
 
     @Override
     public void onEvent(NamespaceClient client, String eventName, List<Object> args, AckRequest ackRequest) {
+        Debug.log("EventManager", "Received Event: " + eventName);
         Peer peer = PeerManager.getPeer(client.getSessionId().toString());
         peer.getEventListener().event(eventName, args.toArray());
     }
