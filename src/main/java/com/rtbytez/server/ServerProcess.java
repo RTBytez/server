4fc2f9ebbda79ab121c1883a295a01d9e9bdb458
CzE <cze@clashmc.co>
2020-03-20 02:02:13 -0400

    Added arguments to main method

--- src/main/java/com/rtbytez/server/ServerProcess.java
+++ src/main/java/com/rtbytez/server/ServerProcess.java
@@ -8,22 +8,49 @@ import com.rtbytez.server.events.io.ConnectionEvent;
 import com.rtbytez.server.events.io.DisconnectionEvent;
 import com.rtbytez.server.packethandler.RTBytezJsonSupport;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * The birth of the RTBytez Server
  */
 public class ServerProcess {
 
-    public static void main(String[] args) {
-        //TODO: Make this mess look better and make args instead of hard-coded values
+    public static void main(String[] rawArgs) {
+        List<String> args = Arrays.asList(rawArgs);
+        if (String.join(" ", args).contains("help")) {
+
+            System.out.println("RTBytez Server");
+            System.out.println("Arguments are as follows");
+            System.out.println("-bind=<IP> | Bind to an ip address (Default=All Interfaces)");
+            System.out.println("-port=<PORT> | Set server port number (Default=5623)");
+            System.out.println("Example use: java -jar rtbytez-server.jar -bind=127.0.0.1 -port=5623");
+
+            System.exit(0);
+            return;
+        }
+
+        String address = "0.0.0.0";
+        int port = 5623;
+
+        for (String str : args) {
+            if (str.startsWith("-bind=")) {
+                address = str.substring(6);
+            }
+            if (str.contains("-port=")) {
+                port = Integer.parseInt(str.substring(6));
+            }
+        }
+
         Configuration config = new Configuration();
-        config.setHostname("127.0.0.1");
-        config.setPort(1338);
+        config.setHostname(address);
+        config.setPort(port);
         SocketConfig socketConfig = new SocketConfig();
         socketConfig.setReuseAddress(true);
         config.setSocketConfig(socketConfig);
         config.setJsonSupport(new RTBytezJsonSupport());
         SocketIOServer server = new SocketIOServer(config);
-        Console.log("SERVER", "Starting...");
+        Console.log("SERVER", "Starting on " + address + ":" + port + " ...");
         server.addConnectListener(new ConnectionEvent());
         server.addDisconnectListener(new DisconnectionEvent());
         server.addEventInterceptor(new EventManager());
