cbed1ba53ce6dcf617320cf7494678666133d739
CzE <cze@clashmc.co>
2020-04-19 23:49:59 -0400

    Finished RTPFileRemoveLine

--- src/main/java/com/rtbytez/server/common/comms/packets/file/broadcasts/RTPFileRemoveLine.java
+++ src/main/java/com/rtbytez/server/common/comms/packets/file/broadcasts/RTPFileRemoveLine.java
@@ -3,4 +3,32 @@ package com.rtbytez.server.common.comms.packets.file.broadcasts;
 import com.rtbytez.server.common.comms.packets.RTPacket;
 
 public class RTPFileRemoveLine extends RTPacket {
+
+    protected RTPFileRemoveLine() {
+    }
+
+    public RTPFileRemoveLine(String header, String roomId, String peerId, String filePath, String lineId) {
+        super(header);
+        setShortCode("FileAddLine");
+        put("roomId", roomId);
+        put("peerId", peerId);
+        put("filePath", filePath);
+        put("lineId", lineId);
+    }
+
+    public String getRoomId() {
+        return getString("roomId");
+    }
+
+    public String getPeerId() {
+        return getString("peerId");
+    }
+
+    public String getFilePath() {
+        return getString("filePath");
+    }
+
+    public String getLineId() {
+        return getString("lineId");
+    }
 }
